548503f56849a483d91965f11ec4ea0392716e09
sarah-shtawi <sarah.sht03@gmail.com>
2024-06-01 13:46:28 +0300

    crud

---
+++ app crud/Controllers/DepartmentController.cs
@@ -0,0 +1,40 @@
+﻿using app_crud.Data;
+using app_crud.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app_crud.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var departments = context.Departments.ToList();
+            return View("Index", departments);
+        }
+
+        public IActionResult Details(int id)
+        {
+           var dep =  context.Departments.Find(id);
+            return View("Details",dep);
+        }
+        public IActionResult Create()
+        {
+            return View("CreateDep");
+        }
+        public IActionResult AddDep(Department dep )
+        {
+            context.Departments.Add(dep);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id) 
+        {
+            var dep = context.Departments.Find(id);
+            context.Departments.Remove(dep);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ app crud/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using app_crud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace app_crud.Data
+{
+    public class ApplicationDbContext :DbContext 
+    {
+       public DbSet <Department> Departments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=db5365.public.databaseasp.net; Database=db5365; User Id=db5365; Password=B-t72Y?pA=w8; Encrypt=False; MultipleActiveResultSets=True;");
+        }
+
+
+
+    }
+}
